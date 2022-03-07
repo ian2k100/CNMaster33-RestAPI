67daef01d96832bf0b1da53f763f68f1ddc17723
ian2k100 <ianrmukoma@gmail.com>
2022-03-07 04:40:29 +0000

    Second Form

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../user/userModel.js");
 
 exports.hashPass = async (req, res, next) => {
@@ -17,12 +18,26 @@ exports.hashPass = async (req, res, next) => {
 exports.decryptPassword = async(req, res, next) => {
     try { 
         const userInfo = await User.findOne({ username: req.body.username });
-        if (await bcrypt.compare(req.body.password, userinfo.password)){
+        if (await bcrypt.compare(req.body.password, userInfo.password)){
             req.user = userInfo;
+            next();
         } else {
             res.status(500).send({ message: "You have entered the wrong password"})
         }
     } catch (error) {
+        console.log(error);
+        res.status(500).send({ err: error.message})
+    }
+}
+
+exports.tokenAuth = async (req,res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer", "");
+        const decoded = await jwt.verify(token, process.env.SECRET);
+        const user = await User.findById(decoded._id);
+        req.user = user;
+        next(); 
+    } catch (error){
         console.log(error);
         res.status(500).send({ err: error.message})
     }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors")
 const userRouter = require("./user/userRoutes");
 const gamesRouter = require("./games/gameRoutes");
 const app = express();
 const port = 5001; 
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(userRouter);
 app.use(gamesRouter);
